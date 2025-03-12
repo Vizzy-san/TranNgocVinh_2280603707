b8c7bb359595307658641b931132dd232d466fe8
unknown <vinhkidboy137@gmail.com>
2025-03-12 16:16:29 +0700

    add lab03_carsar add lab03_rsa

--- lab02/app.py
+++ lab02/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from cipher.caesar import CaesarCipher # Giả sử có module caesar.py
+from cipher.vigenere import VigenereCipher
 
 app = Flask(__name__)
 
@@ -25,6 +26,27 @@ def caesar_decrypt():
     key = int(request.form['inputKeyCipher'])
     caesar = CaesarCipher()
     decrypted_text = caesar.decrypt_text(text, key)
+    return f"text: {text}<br/>key: {key}<br/>decrypted text: {decrypted_text}"
+                                # Vigenere
+
+@app.route("/vigenere")
+def vigenere():
+    return render_template('vigenere.html')
+
+@app.route("/encrypt", methods=['POST'])
+def vigenere_encrypt():
+    text = request.form['inputPlainText']
+    key = request.form['inputKeyPlain']
+    vigenere = VigenereCipher()
+    encrypted_text = vigenere.encrypt_text(text, key)
+    return f"text: {text}<br/>key: {key}<br/>encrypted text: {encrypted_text}"
+
+@app.route("/decrypt", methods=['POST'])
+def vigenere_decrypt():
+    text = request.form['inputCipherText']
+    key = request.form['inputKeyCipher']
+    vigenere = VigenereCipher()
+    decrypted_text = vigenere.decrypt_text(text, key)
     return f"text: {text}<br/>key: {key}<br/>decrypted text: {decrypted_text}"
 
 # main function
